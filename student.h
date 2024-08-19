36764fd24e550bfe6586713cee7d771c80351271
sbirbas <sophia@track5media.com>
2024-08-19 19:19:30 -0400

    create student class with vars, getters, setters & print function

---
+++ student.h
@@ -0,0 +1,40 @@
+#include <string>
+#include "degree.h"
+
+using namespace std;
+
+class Student {
+private:
+    string studentID;
+    string firstName;
+    string lastName;
+    string email;
+    int age;
+    int daysInCourse[3];
+    DegreeProgram degreeProgram;
+
+public:
+    // constructor
+    Student(string studentID, string firstName, string lastName, string email, int age, int daysInCourse[], DegreeProgram degreeProgram);
+
+    // accessors
+    string getStudentID();
+    string getFirstName();
+    string getLastName();
+    string getEmail();
+    int getAge();
+    int* getDaysInCourse();
+    DegreeProgram getDegreeProgram();
+
+    // mutators
+    void setStudentID(string studentID);
+    void setFirstName(string firstName);
+    void setLastName(string lastName);
+    void setEmail(string email);
+    void setAge(int age);
+    void setDaysInCourse(int daysInCourse[]);
+    void setDegreeProgram(DegreeProgram degreeProgram);
+
+    // print function
+    void print();
+};
